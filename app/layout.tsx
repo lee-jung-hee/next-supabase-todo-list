148c1ea4300ff204c35b935070dd36b72e8a8d74
lee-jung-hee <zhongxi710@gmail.com>
2025-08-29 14:28:57 +0900

    Implement default page and style

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,20 +12,19 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ThemeProvider>
-      {/* @ts-ignore */}
-      <html lang="en">
-        <head>
-          <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
-            integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
-            crossOrigin="anonymous"
-            referrerPolicy="no-referrer"
-          />
-        </head>
+    <html lang="en">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+          integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+      </head>
+      <ThemeProvider>
         <body className={inter.className}>{children}</body>
-      </html>
-    </ThemeProvider>
+      </ThemeProvider>
+    </html>
   );
 }
